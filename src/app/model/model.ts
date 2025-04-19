60cfe0a6f7dabab64301cc6666d09cc03964478e
release_the_kraken <wmichal13@wp.pl>
2025-04-19 18:57:37 +0200

    basic version working locally

--- src/app/model/model.ts
+++ src/app/model/model.ts
@@ -1,3 +1,4 @@
 export type Word = {
-  word: string;
+  id: number
+  word: string
 }
--- src/app/my-input/my-input.component.ts
+++ src/app/my-input/my-input.component.ts
@@ -1,10 +1,11 @@
+import { Word } from './../model/model';
 import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import { CommonModule } from "@angular/common";
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from "@angular/forms";
-import { Word } from '../model/model';
+import { CommonService } from '../service/common.service';
 
 @Component({
   selector: 'app-my-input',
@@ -14,11 +15,32 @@ import { Word } from '../model/model';
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
-    
+    MatButtonModule
   ],
   templateUrl: './my-input.component.html',
   styleUrl: './my-input.component.css'
 })
 export class MyInputComponent {
-  word!: Word;
+  word: Word = this.getDefaultWord()
+
+  constructor(private service: CommonService){}
+
+  getDefaultWord(): Word{
+    return Object.assign({}, {
+      id: 0,
+      word: ""
+    })
+  }
+
+  handleSubmit(){
+    this.service.create(this.word)
+    .subscribe({
+      next: () => {
+        console.log("Added word: " + this.word.id + " | " + this.word.word)
+      },
+      error: (err) => {
+        console.log("ERROR: " + this.word.word)
+      }
+    })
+  }
 }
